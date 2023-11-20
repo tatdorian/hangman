08908ddccbacad8f388fcf2890260cd08345da0c
tdorian <dorian.tatoulian@ynov.com>
2023-11-20 19:06:02 +0100

    sans ascii art

--- hangman.go
+++ hangman.go
@@ -21,21 +21,17 @@ func LoadWords(filename string) ([]string, error) {
 		return nil, err
 	}
 	defer file.Close()
-
 	var words []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		words = append(words, scanner.Text())
 	}
-
 	return words, scanner.Err()
 }
-
 func RandomWord(words []string) string {
 	randIndex := rand.Intn(len(words))
 	return words[randIndex]
 }
-
 func RevealLetter(word, display, letter string) string {
 	var builder strings.Builder
 	for i := 0; i < len(word); i++ {
@@ -47,18 +43,15 @@ func RevealLetter(word, display, letter string) string {
 	}
 	return builder.String()
 }
-
 func LoadHangmanPositions(filename string) ([]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
-
 	var positions []string
 	scanner := bufio.NewScanner(file)
 	var position strings.Builder
-
 	for scanner.Scan() {
 		if scanner.Text() == "" {
 			positions = append(positions, position.String())
@@ -68,10 +61,8 @@ func LoadHangmanPositions(filename string) ([]string, error) {
 		}
 	}
 	positions = append(positions, position.String())
-
 	return positions, scanner.Err()
 }
-
 func DisplayHangman(positions []string, essai int) {
 	if essai < 0 || essai >= len(positions) {
 		return
--- main/main.go
+++ main/main.go
@@ -11,47 +11,65 @@ func main() {
 	wordsFile := "words.txt"
 	words, err := hangman.LoadWords(wordsFile)
 	if err != nil {
-		fmt.Printf("Erreur pour charger le mot %s: %v\n", wordsFile, err)
+		fmt.Printf("Error loading words from %s: %v\n", wordsFile, err)
 		return
 	}
 
+	guessedLetters := []string{}
 	word := hangman.RandomWord(words)
-	essai := 10
+	attempts := 10
 	display := revealRandomLetters(word)
 
 	PositionsFile := "hangman.txt"
 	Positions, err := hangman.LoadHangmanPositions(PositionsFile)
 	if err != nil {
-		fmt.Printf("Erreur pour afficher les positions : %v\n", err)
+		fmt.Printf("Error loading hangman positions: %v\n", err)
 		return
 	}
 
-	fmt.Println("Bonne Chance. Vous avez 10 essais")
+	fmt.Println("Good Luck. You have 10 attempts")
 	fmt.Println(display)
 
-	for essai > 0 {
-		fmt.Print("")
-		fmt.Print("Choose a letter: ")
-		var letter string
-		fmt.Scanln(&letter)
-		if len(letter) != 1 {
-			fmt.Println("Please enter a single letter.")
-			continue
-		}
-		if strings.Contains(word, letter) {
-			display = hangman.RevealLetter(word, display, letter)
-			fmt.Println(display)
-			if display == word {
-				fmt.Println("Bravo !")
+	for attempts > 0 {
+		fmt.Print("Choose a letter or a word: ")
+		var input string
+		fmt.Scanln(&input)
+
+		if len(input) == 1 {
+			letter := input
+			if contains(guessedLetters, letter) {
+				fmt.Printf("You have already guessed this letter '%s'.\n", letter)
+				continue
+			}
+			guessedLetters = append(guessedLetters, letter)
+
+			if strings.Contains(word, letter) {
+				display = hangman.RevealLetter(word, display, letter)
+				fmt.Println(display)
+				if display == word {
+					fmt.Println("Congratulations!")
+					return
+				}
+			} else {
+				attempts--
+				fmt.Printf("Letter not found, %d attempts remaining:\n", attempts)
+				hangman.DisplayHangman(Positions, 10-attempts)
+			}
+		} else if len(input) >= 2 {
+			if input == word {
+				fmt.Println("Congratulations! You guessed the word.")
 				return
+			} else {
+				attempts -= 2
+				fmt.Printf("Incorrect word, %d attempts remaining:\n", attempts)
+				hangman.DisplayHangman(Positions, 10-attempts)
 			}
 		} else {
-			fmt.Printf("Not present in the word, %d attempts remaining\n", essai)
-			hangman.DisplayHangman(Positions, 10-essai)
-			essai--
+			fmt.Println("Please enter a valid letter or word.")
 		}
 	}
-	fmt.Printf("You've run out of attempts. The word was %s.\n", word)
+
+	fmt.Printf("You have run out of attempts. The word was %s.\n", word)
 }
 func revealRandomLetters(word string) string {
 	n := len(word)/2 - 1
@@ -69,7 +87,14 @@ func generateRandomIndices(length, n int) []int {
 	if n > length {
 		n = length
 	}
-
 	indices := rand.Perm(length)[:n]
 	return indices
+}
+func contains(slice []string, letter string) bool {
+	for _, l := range slice {
+		if l == letter {
+			return true
+		}
+	}
+	return false
 }
